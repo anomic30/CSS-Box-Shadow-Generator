efbf9fc905e14a6727a65d344ff56fa99ea7cedb
Anom Chakravorty <63467479+anomic30@users.noreply.github.com>
2021-02-28 16:43:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ function inputChanged(e){
         e.target.value
     )
 }
+
+var slider1 = document.getElementById("x-position");
+var output1 = document.getElementById("x-value");
+output1.innerHTML = slider1.value;
+
+slider1.oninput = function() {
+  output1.innerHTML = this.value;
+}
+
+var slider2 = document.getElementById("y-position");
+var output2 = document.getElementById("y-value");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function() {
+  output2.innerHTML = this.value;
+}
+
+var slider3 = document.getElementById("blur");
+var output3 = document.getElementById("blur-value");
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function() {
+  output3.innerHTML = this.value;
+}
+
+var slider4 = document.getElementById("spread");
+var output4 = document.getElementById("spread-value");
+output4.innerHTML = slider4.value;
+
+slider4.oninput = function() {
+  output4.innerHTML = this.value;
+}
